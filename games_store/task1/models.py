59d56a1cc051dfe8d0ef25c05454cc73e860e95e
Aleksandr <kostromin63@mail.ru>
2024-11-21 16:32:47 +0300

    Create new models: 'Bayer', 'Game'. Migrations in BD

--- games_store/task1/models.py
+++ games_store/task1/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.forms import DecimalField
 
-# Create your models here.
+
+class Bayer(models.Model):
+    name = models.CharField(max_length=30)# username
+    balance = models.DecimalField(decimal_places=2, max_digits=9)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=7, decimal_places=2) # в рублях с копейками
+    size = models.DecimalField(max_digits=7, decimal_places=1) # в мегабайтах
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Bayer, related_name='bayers')
