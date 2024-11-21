53b0f6fd0a22ef6ef78df2f8d8beb257a45a0c1e
Aleksandr <kostromin63@mail.ru>
2024-11-21 17:58:49 +0300

    New create models: "Buer" and "Game"

--- games_store/task1/models.py
+++ games_store/task1/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 
-# class Buyer(models.Model):
-#     name = models.CharField(max_length=30)# username
-#     balance = models.DecimalField(decimal_places=2, max_digits=9)
-#     age = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.name
+class Buyer(models.Model):
+    name = models.CharField(max_length=30)# username
+    balance = models.DecimalField(decimal_places=2, max_digits=9)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
-# class Game(models.Model):
-#     title = models.CharField(max_length=200)
-#     cost = models.DecimalField(max_digits=7, decimal_places=2) # в рублях с копейками
-#     size = models.DecimalField(max_digits=7, decimal_places=1) # в мегабайтах
-#     description = models.TextField()
-#     age_limited = models.BooleanField(default=False)
-#     buyer = models.ManyToManyField(Buyer, related_name='buyers')
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(max_digits=7, decimal_places=2) # в рублях с копейками
+    size = models.DecimalField(max_digits=7, decimal_places=1) # в мегабайтах
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='buyers')
