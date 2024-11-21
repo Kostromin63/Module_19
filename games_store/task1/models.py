5db48f0e0abd33511131c8ef819020fb5e09ba2c
Aleksandr <kostromin63@mail.ru>
2024-11-21 17:42:59 +0300

    Removing database creation errors

--- games_store/task1/models.py
+++ games_store/task1/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-from django.forms import DecimalField
 
 
-class Bayer(models.Model):
-    name = models.CharField(max_length=30)# username
-    balance = models.DecimalField(decimal_places=2, max_digits=9)
-    age = models.IntegerField()
+# class Buyer(models.Model):
+#     name = models.CharField(max_length=30)# username
+#     balance = models.DecimalField(decimal_places=2, max_digits=9)
+#     age = models.IntegerField()
+#
+#     def __str__(self):
+#         return self.name
 
-    def __str__(self):
-        return self.name
 
-
-class Game(models.Model):
-    title = models.CharField(max_length=200)
-    cost = models.DecimalField(max_digits=7, decimal_places=2) # в рублях с копейками
-    size = models.DecimalField(max_digits=7, decimal_places=1) # в мегабайтах
-    description = models.TextField()
-    age_limited = models.BooleanField(default=False)
-    buyer = models.ManyToManyField(Bayer, related_name='bayers')
+# class Game(models.Model):
+#     title = models.CharField(max_length=200)
+#     cost = models.DecimalField(max_digits=7, decimal_places=2) # в рублях с копейками
+#     size = models.DecimalField(max_digits=7, decimal_places=1) # в мегабайтах
+#     description = models.TextField()
+#     age_limited = models.BooleanField(default=False)
+#     buyer = models.ManyToManyField(Buyer, related_name='buyers')
